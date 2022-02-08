6976a8d8d3874d6f2e03a317464b3b59bdcf636a
Matt <matthewtansley3@gmail.com>
2022-02-08 11:54:35 +0000

    created rooms class and added its properties (rn cant get an idea of how to proceed)

---
+++ textgame2/Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace textgame2
+{
+    class Room //class needs properties e.g (class car: string colour)
+    {
+        public int height;
+        public int width;
+        public string puzzle;
+
+        public int x, y;
+
+
+    }
+}
